9adb85432b2ae92dfd1c6c09d1b837ea9068b074
maliilma <maliyoujian@gmail.com>
2020-09-24 21:59:24 +0300

    add route and link

--- todo/src/components/layout/Header.js
+++ todo/src/components/layout/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <header style={headerStyle}>
             <h3>Todo List</h3>
+            <Link to="/" style={linkStyle}>Home</Link>
+            <Link to="/about" style={linkStyle}>About</Link>
         </header>
     )
 }
@@ -12,5 +15,10 @@ const headerStyle = {
     background: 'black',
     color: 'white',
     textAlign: 'center'
+}
+
+const linkStyle = {
+    color: 'yellow',
+    margin: '5px'
 }
 export default Header;
---
+++ todo/src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>Todo list app v1.0.0 using react</p>
+        </React.Fragment>
+    )
+}
