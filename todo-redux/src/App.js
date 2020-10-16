1b52d25e94632d3c4776ea23c90c5563b8e80ef2
maliilma <maliyoujian@gmail.com>
2020-10-16 17:38:23 +0300

    addTodo dummy

--- todo-redux/src/App.js
+++ todo-redux/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import AddTodo from './components/AddTodo'
 import TodosList from './components/TodoList'
 
 function App() {
   return (
       <div>
         <h4>Todo List</h4>
+        <AddTodo />
         <TodosList />
       </div>
   );
---
+++ todo-redux/src/components/AddTodo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AddTodo = ({ dispatch }) => {
+    let input
+    return (
+        <div>
+            <form>
+                <input />
+                <button type="submit">Add Todo</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddTodo
