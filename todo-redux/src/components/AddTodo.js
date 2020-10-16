bd1a0052f8e29c8f9373a9a5c9093ad8353deb30
maliilma <maliyoujian@gmail.com>
2020-10-16 21:36:19 +0300

    dispatch addTodo action with action creator

--- todo-redux/src/components/AddTodo.js
+++ todo-redux/src/components/AddTodo.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { addTodo } from '../actions'
 
 const AddTodo = ({ dispatch }) => {
     let input
     return (
         <div>
-            <form>
-                <input />
+            <form
+                onSubmit={e => {
+                    e.preventDefault()
+                    if (!input.value.trim()) {
+                        return
+                    }
+                    dispatch(addTodo(input.value))
+                    input.value = ''
+                }}
+            >
+                { /* Callback Refs:  https://reactjs.org/docs/refs-and-the-dom.html#callback-refs0 */ }
+                <input ref={node => (input = node)}/>
                 <button type="submit">Add Todo</button>
             </form>
         </div>
