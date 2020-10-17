dbab66e92c8a862f4621096d6e4c251f6e419484
maliilma <maliyoujian@gmail.com>
2020-10-17 14:53:40 +0300

    Fixed todoList

--- todo-redux/src/App.js
+++ todo-redux/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import AddTodo from './components/AddTodo'
-import TodosList from './components/TodoList'
+import TodoList from './components/TodoList'
 
 function App() {
   return (
       <div>
         <h4>Todo List</h4>
         <AddTodo />
-        <TodosList />
+        <TodoList />
       </div>
   );
 }
--- todo-redux/src/components/TodoList.js
+++ todo-redux/src/components/TodoList.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Todo from './Todo'
 
-const TodoList = ({ todos }) => (
-    <ul>
-        {(todos || []).map(todo => (
-            <Todo todo />
-        ))}
-    </ul>
-)
+const TodoList = ({ todos }) => {
+    console.log("todos:", todos)
+    return (
+        <div>
+            {todos.map(todo => (
+                <div key={todo.id}>{todo.text}-{todo.completed}-{todo.id}</div>
+            ))}
+        </div>
+    )
+}
 
-export default TodoList
+const mapStateToProps = state => ({
+    todos: state.todos
+})
+
+export default connect(mapStateToProps)(TodoList)
