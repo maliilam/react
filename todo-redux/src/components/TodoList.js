a24e35e85b370174b67a2ca47f0e9268caf6c336
maliilma <maliyoujian@gmail.com>
2020-10-24 20:32:59 +0300

    change TodoList to react component

--- todo-redux/src/components/TodoList.js
+++ todo-redux/src/components/TodoList.js
@@ -1,34 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Todo from './Todo'
 import { requestGetTodos, toggleTodo, updateTodo, deleteTodo, Filters } from '../actions'
 
-const TodoList = ({ todos, toggleTodo, updateTodo, deleteTodo, filter }) => {
-    const filterTodo = (todo) => {
-        switch(filter) {
-            case Filters.SHOW_COMPLETED:
-                return todo.completed === true
-            case Filters.SHOW_ACTIVE:
-                return todo.completed === false
-            default:
-                return true            
+class TodoList extends Component {
+    render() {
+        const { todos, filter, requestGetTodos, toggleTodo, updateTodo, deleteTodo } = this.props
+        
+        const filterTodo = (todo) => {
+            switch(filter) {
+                case Filters.SHOW_COMPLETED:
+                    return todo.completed === true
+                case Filters.SHOW_ACTIVE:
+                    return todo.completed === false
+                default:
+                    return true            
+            }
         }
-    }
 
-    return (
-        <div>
-            {todos.filter(todo => filterTodo(todo))
-                .map(todo => (
-                <Todo 
-                    key={todo.id} 
-                    {...todo} 
-                    toggleTodo={() => toggleTodo(todo.id)}
-                    updateTodo={text => updateTodo(todo.id, text)}
-                    deleteTodo={() => deleteTodo(todo.id)}
-                />
-            ))}
+        return (
+            <div>
+                {todos.filter(todo => filterTodo(todo))
+                    .map(todo => (
+                    <Todo 
+                        key={todo.id} 
+                        {...todo} 
+                        toggleTodo={() => toggleTodo(todo.id)}
+                        updateTodo={text => updateTodo(todo.id, text)}
+                        deleteTodo={() => deleteTodo(todo.id)}
+                    />
+                ))}
         </div>
-    )
+        )
+    }
 }
 
 const mapStateToProps = state => ({
@@ -37,6 +41,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
+    requestGetTodos: () => dispatch(requestGetTodos),
     toggleTodo: id => dispatch(toggleTodo(id)),
     updateTodo: (id, text) => dispatch(updateTodo(id, text)),
     deleteTodo: id => dispatch(deleteTodo(id))
