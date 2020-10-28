6f8b0ac778e0a55d362eb6e7a7eba9fdd0bfe62d
maliilma <maliyoujian@gmail.com>
2020-10-28 17:13:28 +0200

    Try the simple Router

--- todo-redux/src/App.js
+++ todo-redux/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import './App.css';
 import AddTodo from './components/AddTodo'
 import TodoList from './components/TodoList'
 import Filter from './components/Filter'
+import About from './components/About'
 
 function App() {
   return (
-      <div>
+    <Router>
+      <NavLink exact to='/'>Hmoe</NavLink>&nbsp;&nbsp;
+      <NavLink exact to='/about'>About</NavLink>
+      <Route exact path="/">
         <h4>Todo List</h4>
         <Filter />
         <AddTodo />
         <TodoList />
-      </div>
+      </Route>
+      <Route path="/about" component={About} />
+    </Router>
   );
 }
 
